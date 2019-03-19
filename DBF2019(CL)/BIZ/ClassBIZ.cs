6321ff19d82edceb4d2ded0653fb6432fbe9e0a7
Christoffer Lorenzen <chri603v@edu.campusvejle.dk>
2019-03-19 17:43:47 +0100

    referance

--- DBF2019(CL)/BIZ/ClassBIZ.cs
+++ DBF2019(CL)/BIZ/ClassBIZ.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using REPO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,13 +7,17 @@ using System.Threading.Tasks;
 
 namespace BIZ
 {
-    class ClassBIZ
+    public class ClassBIZ
     {
+        ClassREPO classrepo = new ClassREPO();
 
         public ClassBIZ()
         {
-
-        }       
-
+            
+        }   
+        public void repogetset()
+        {
+            
+        }
     }
 }
